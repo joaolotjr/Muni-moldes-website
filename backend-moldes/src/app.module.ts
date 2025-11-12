cf2ac8c492e5cac37341604e80f29d898dbc7697
João Carlos Lot Junior <joaolotjr@gmail.com>
2025-11-12 18:59:37 -0300

    Feat: Criação do BD

--- backend-moldes/src/app.module.ts
+++ backend-moldes/src/app.module.ts
@@ -1,9 +1,23 @@
+// backend-moldes/src/app.module.ts
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // Mapeado do docker-compose
+      port: 5432,
+      username: 'user_admin',
+      password: 'senha_segura',
+      database: 'catalogo_db',
+      autoLoadEntities: true, // Carrega as entidades automaticamente
+      synchronize: true, // APENAS PARA DESENVOLVIMENTO: Cria as tabelas
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
